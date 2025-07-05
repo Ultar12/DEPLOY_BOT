70ed0dee7d0fd1ffb0a2c29627e874e8bb3eccfc
ultar1 <138212013+ultar1@users.noreply.github.com>
2025-07-05 11:45:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -582,47 +582,50 @@ if (action === 'setvar') {
   });
 }
 
-  // varselect
-  if (action === 'varselect') {
-    const varKey = p, appName = extra;
-    if (['AUTOSTATUSVIEW','ALWAYSONLINE','ANTIDELETE'].includes(varKey)) {
-      return bot.sendMessage(cid, Set ${varKey} to:, {
-        reply_markup: {
-          inline_keyboard: [[
-            { text: 'true', callback_data: setvarbool:${varKey}:${appName}:true },
-            { text: 'false', callback_data: setvarbool:${varKey}:${appName}:false }
-          ]]
-        }
-      });
-    }
-    userStates[cid] = { step: 'SETVARENTERVALUE', data: { APPNAME: appName, VARNAME: varKey } };
-    return bot.sendMessage(cid, Enter new value for ${varKey}:);
-  }
-
-  // setvarbool
-  if (action === 'setvarbool') {
-    const varKey = p, appName = extra, flagVal = flag === 'true';
-    let newVal;
-    if (varKey === 'AUTOSTATUSVIEW') newVal = flagVal ? 'no-dl' : 'false';
-    else if (varKey === 'ANTI_DELETE') newVal = flagVal ? 'p' : 'false';
-    else newVal = flagVal ? 'true' : 'false';
-    try {
-      await axios.patch(
-        https://api.heroku.com/apps/${appName}/config-vars,
-        { [varKey]: newVal },
-        { headers: {
-            Authorization: Bearer ${HEROKUAPIKEY},
-            Accept: 'application/vnd.heroku+json; version=3',
-            'Content-Type': 'application/json'
-          }}
-      );
-      if (varKey === 'SESSION_ID') {
-        await updateUserSession(cid, appName, newVal);
+// varselect
+if (action === 'varselect') {
+  const varKey = p, appName = extra;
+  if (['AUTOSTATUSVIEW', 'ALWAYSONLINE', 'ANTIDELETE'].includes(varKey)) {
+    return bot.sendMessage(cid, `Set ${varKey} to:`, {
+      reply_markup: {
+        inline_keyboard: [[
+          { text: 'true', callback_data: `setvarbool:${varKey}:${appName}:true` },
+          { text: 'false', callback_data: `setvarbool:${varKey}:${appName}:false` }
+        ]]
       }
-      return bot.sendMessage(cid, ${varKey} updated to ${newVal});
-    } catch (e) {
-      return bot.sendMessage(cid, Error: ${e.message});
-    }
+    });
   }
-});
-`
+  userStates[cid] = { step: 'SETVARENTERVALUE', data: { APPNAME: appName, VARNAME: varKey } };
+  return bot.sendMessage(cid, `Enter new value for ${varKey}:`);
+}
+
+// setvarbool
+if (action === 'setvarbool') {
+  const varKey = p, appName = extra, flagVal = flag === 'true';
+  let newVal;
+  if (varKey === 'AUTOSTATUSVIEW') newVal = flagVal ? 'no-dl' : 'false';
+  else if (varKey === 'ANTI_DELETE') newVal = flagVal ? 'p' : 'false';
+  else newVal = flagVal ? 'true' : 'false';
+
+  try {
+    await axios.patch(
+      `https://api.heroku.com/apps/${appName}/config-vars`,
+      { [varKey]: newVal },
+      {
+        headers: {
+          Authorization: `Bearer ${HEROKUAPIKEY}`,
+          Accept: 'application/vnd.heroku+json; version=3',
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    if (varKey === 'SESSION_ID') {
+      await updateUserSession(cid, appName, newVal);
+    }
+
+    return bot.sendMessage(cid, `${varKey} updated to ${newVal}`);
+  } catch (e) {
+    return bot.sendMessage(cid, `Error: ${e.message}`);
+  }
+}
