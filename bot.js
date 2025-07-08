1a4f3d19759617288884a31a7fc7c9da0e204c31
ultar1 <138212013+ultar1@users.noreply.github.com>
2025-07-08 04:32:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -917,7 +917,7 @@ bot.onText(/^\/send (\d+) ([a-zA-Z0-9]{8})$/, async (msg, match) => { // Updated
     // Send the pairing code to the original user
     await bot.sendMessage(targetUserId,
         `Your Pairing-code is:\n` +
-        '```\n' + pairingCode + '\n```\n' + // Corrected line
+        '```\n' '''+ pairingCode''' + '\n```\n' + // Corrected line
         `Copy the code and paste it to your WhatsApp linked device ASAP!`,
         { parse_mode: 'Markdown' }
     );
