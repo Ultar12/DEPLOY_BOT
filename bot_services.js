4d93d0df7ab049c6de7839dae9784560cb2344e6
ultar1 <138212013+ultar1@users.noreply.github.com>
2026-05-21 03:33:11 +0100

    Update bot_services.js

--- bot_services.js
+++ bot_services.js
@@ -2100,6 +2100,7 @@ async function buildWithProgress(targetChatId, vars, isFreeTrial, isRestore, bot
             console.log(`[Build] Setting full buildpacks (ffmpeg, nodejs) for ${botType} bot: ${appName}`);
             buildpacksToInstall = [
   { buildpack: 'https://github.com/heroku/heroku-buildpack-apt' },
+  { buildpack: 'https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest.git' },
   { buildpack: 'heroku/nodejs' }
 ];
 
